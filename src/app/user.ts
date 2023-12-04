96159544e1f38ac7b34611bbd9664e2338df5532
KACPER GORALCZYK <kg544inl@inlumine.ual.es>
2023-12-04 20:25:18 +0100

    dodatkowe funkcje w serwisie, przesylanie danych miedzy komponentami, drobne poprawki w formularzu, widok home

--- src/app/user.ts
+++ src/app/user.ts
@@ -1,11 +1,13 @@
-import { Goal } from "./goal";
+import { Goal } from "./goal.enum";
 
 export interface User {
-    username: string;
-    password: string;
-    name: string;
-    weight: number;
-    height: number;
-    age: number;
-    goal: Goal;
+    username?: string;
+    password?: string;
+    name?: string;
+    weight?: number;
+    height?: number;
+    age?: number;
+    goal?: Goal;
+    gender?: string;
+    activityLevel?: string;
 }
