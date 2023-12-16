3f9df8ea14a43b4d5aff2293a74c741297dca800
KACPER GORALCZYK <kg544inl@inlumine.ual.es>
2023-12-16 20:18:19 +0100

    dodano nowy widok dnia

--- src/app/data/mock/ingredient.data.ts
+++ src/app/data/mock/ingredient.data.ts
@@ -25,5 +25,66 @@ export const INGREDIENTS:Ingredient[] = [
       { type: nutritionType.fats, value: 81 },
       { type: nutritionType.proteins, value: 1 }
     ]
+  },
+
+  {
+    name: "Budyn",
+    nutritionals: [
+      { type: nutritionType.fats, value: 20 },
+      { type: nutritionType.proteins, value: 10 },
+      { type: nutritionType.sugars, value:10 }
+    ]
+  },
+
+  {
+    name: "Kotlet Schabowy",
+    nutritionals: [
+      { type: nutritionType.fats, value: 30 },
+      { type: nutritionType.proteins, value: 20 },
+      { type: nutritionType.salt, value:2 }
+    ]
+  },
+
+  {
+    name: "Ziemniaki",
+    nutritionals: [
+      { type: nutritionType.carbohydrates, value: 30 },
+      { type: nutritionType.proteins, value: 3 },
+      { type: nutritionType.salt, value:1 }
+    ]
+  },
+
+  {
+    name: "Surowka",
+    nutritionals: [
+      { type: nutritionType.calorific_value, value: 50 },
+      { type: nutritionType.proteins, value: 13 },
+      { type: nutritionType.salt, value:1 }
+    ]
+  },
+
+  {
+    name: "Tunczyk",
+    nutritionals: [
+      { type: nutritionType.fats, value: 20 },
+      { type: nutritionType.proteins, value: 20 },
+      { type: nutritionType.salt, value:2 }
+    ]
+  },
+
+  {
+    name: "Sos",
+    nutritionals: [
+      { type: nutritionType.carbohydrates, value: 5 },
+      { type: nutritionType.sugars, value: 5 },
+    ]
+  },
+
+  {
+    name: "Salatka",
+    nutritionals: [
+      { type: nutritionType.proteins, value: 20 },
+      { type: nutritionType.calorific_value, value:40 }
+    ]
   }
 ]
